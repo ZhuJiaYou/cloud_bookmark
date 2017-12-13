a426dd170a3b67802a0bd765e9b5787e0fa5c62c
jinpeng0577 <qq253021731@gmail.com>
2017-12-13 02:11:54 -0800

    Add account activation

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -31,7 +31,15 @@ module SessionsHelper
 
   # 返回当前登录的用户（如果有的话）
   def current_user
-    @current_user ||= User.find_by(id: session[:user_id])
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(:remember, cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
+    end
   end
 
 
