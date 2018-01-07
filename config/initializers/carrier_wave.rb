c26d981df8c47d905c13db82197101c2d18f9719
ZhuJiayou <lxfdbzjy@163.com>
2018-01-07 02:15:20 +0000

    Try to fix pic prb 2

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -4,9 +4,9 @@ if Rails.env.production?
       # Amazon S3 的配置
       :provider              => 'AWS',
       :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
-      :aws_secret_access_key => ENV['S3_SECRET_KEY'],
-      :region                => ENV['S3_REGION']
+      :aws_secret_access_key => ENV['S3_SECRET_KEY']
     }
     config.fog_directory     =  ENV['S3_BUCKET']
   end
 end
+
