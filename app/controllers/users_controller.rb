74b5898b2401ae6c075d899bea078e33277c305b
ZhuJiayou <lxfdbzjy@163.com>
2018-01-28 06:04:54 +0000

    Show bookmark in Multi columns

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,19 +3,19 @@ class UsersController < ApplicationController
   before_action :correct_user,   only: [:edit, :update]
   before_action :admin_user,     only: :destroy
   def index
-        @users = User.paginate(page: params[:page])
+    @users = User.paginate(page: params[:page])
   end
 
   def show
     @user = User.find(params[:id])
-    @bookmarks = @user.bookmarks.paginate(page: params[:page])
+    @bookmarks = @user.bookmarks.paginate(page: params[:page], :per_page => 12)
   end
 
   def new
   @user = User.new
   end
 	
-   def create
+  def create
     @user = User.new(user_params)
     if @user.save
       @user.send_activation_email
