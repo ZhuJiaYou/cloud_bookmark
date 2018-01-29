fe8b04bc067478bc631c51eeb6153e7713db8a62
ZhuJiayou <lxfdbzjy@163.com>
2018-01-29 06:46:58 +0000

    Localize users & Adjust tests to fit

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,22 +6,22 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create!(name:  "Example User",
-             email: "example@railstutorial.org",
-             password:              "foobar",
-             password_confirmation: "foobar",
+User.create!(name:  "Admin",
+             email: "zhujiayou17@mails.ucas.ac.cn",
+             password:              "justforyou",
+             password_confirmation: "justforyou",
              admin: true,
              activated: true,
              activated_at: Time.zone.now)
 
 99.times do |n|
   name  = Faker::Name.name
-  email = "example-#{n+1}@railstutorial.org"
+  email = "example-#{n+1}@cloudbookmark.com"
   password = "password"
   User.create!(name:  name,
                email: email,
                password:              password,
-               password_confirmation: password,             
+               password_confirmation: password,
                activated: true,
                activated_at: Time.zone.now)
 end
--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -12,8 +12,9 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     get users_path
     assert_template 'users/index'
     assert_select 'div.pagination'
-    first_page_of_users = User.paginate(page: 1)
+    first_page_of_users = User.paginate(page: 1)[0..15]
     first_page_of_users.each do |user|
+      print user.name
       assert_select 'a[href=?]', user_path(user), text: user.name
       unless user == @admin
         assert_select 'a[href=?]', user_path(user), text: 'delete'
